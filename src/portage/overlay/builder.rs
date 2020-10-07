dd2c969db6c8781e4484891d38ecf7372afbd198
Chris McDonald <cjmcdonald@chromium.org>
2020-10-07 12:13:05 -0600

    Move portage/overlay.rs to full submodule
    Make a full submodule to organize the types and their builders more
    clearly.
    
    Change-Id: I4d3e37b18a70fc8ef4cd7c5d36b9be56dad97056
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/infra/build/drydock/+/2461539

---
+++ src/portage/overlay/builder.rs
@@ -0,0 +1,111 @@
+use std::{
+    collections::HashMap,
+    convert::TryFrom,
+    sync::{Arc, Mutex},
+};
+
+use anyhow::Context;
+use ignore::WalkState;
+
+use super::{Overlay, OverlayTable};
+
+#[derive(Debug)]
+pub struct OverlayTableBuilder {
+    pub(super) table: Arc<Mutex<OverlayTable>>,
+    pub(super) errs: Arc<Mutex<Vec<anyhow::Error>>>,
+}
+
+impl OverlayTableBuilder {
+    pub fn new() -> Self {
+        Self {
+            table: Arc::new(Mutex::new(OverlayTable::new())),
+            errs: Arc::new(Mutex::new(Default::default())),
+        }
+    }
+}
+
+impl<'s> ignore::ParallelVisitorBuilder<'s> for OverlayTableBuilder {
+    fn build(&mut self) -> Box<dyn ignore::ParallelVisitor + 's> {
+        Box::new(OverlayTablePiece {
+            table: Arc::clone(&self.table),
+            map: HashMap::new(),
+            errs: Arc::clone(&self.errs),
+            local_errs: Default::default(),
+        })
+    }
+}
+
+impl TryFrom<OverlayTableBuilder> for OverlayTable {
+    type Error = anyhow::Error;
+    fn try_from(other: OverlayTableBuilder) -> Result<Self, Self::Error> {
+        let errs = Arc::try_unwrap(other.errs).unwrap().into_inner().unwrap();
+        if !errs.is_empty() {
+            return Err(errs.into_iter().next().unwrap());
+        }
+        Ok(Arc::try_unwrap(other.table).unwrap().into_inner()?)
+    }
+}
+
+#[derive(Debug)]
+pub struct OverlayTablePiece {
+    table: Arc<Mutex<OverlayTable>>,
+    errs: Arc<Mutex<Vec<anyhow::Error>>>,
+    map: HashMap<String, Overlay>,
+    local_errs: Vec<anyhow::Error>,
+}
+
+impl Drop for OverlayTablePiece {
+    fn drop(&mut self) {
+        let mut table = self.table.lock().unwrap();
+        let map = std::mem::replace(&mut self.map, HashMap::new());
+        for (k, v) in map {
+            table.map.insert(k, v);
+        }
+
+        let mut errs = self.errs.lock().unwrap();
+        let local_errs = std::mem::take(&mut self.local_errs);
+        for e in local_errs {
+            errs.push(e);
+        }
+    }
+}
+
+impl ignore::ParallelVisitor for OverlayTablePiece {
+    fn visit(&mut self, entry: Result<ignore::DirEntry, ignore::Error>) -> WalkState {
+        if let Ok(dir) = entry {
+            match Overlay::try_from(dir.path()) {
+                Ok(mut overlay) => match overlay
+                    .parse_profiles()
+                    .with_context(|| format!("Failed while parsing profiles of {}", &overlay.name))
+                {
+                    Ok(_) => {
+                        for p in overlay.profiles.values_mut() {
+                            match p.parse_conf().with_context(|| {
+                                format!(
+                                    "Failed while parsing {:?}:{} conf!",
+                                    dir.path().components().last().unwrap(),
+                                    p.name
+                                )
+                            }) {
+                                Ok(_) => continue,
+                                Err(e) => {
+                                    self.local_errs.push(e);
+                                    return WalkState::Quit;
+                                }
+                            }
+                        }
+                        self.map.insert(overlay.name.clone(), overlay);
+                        WalkState::Skip
+                    }
+                    Err(e) => {
+                        self.local_errs.push(e);
+                        WalkState::Quit
+                    }
+                },
+                Err(_) => WalkState::Continue,
+            }
+        } else {
+            WalkState::Continue
+        }
+    }
+}
--- src/portage/overlay/mod.rs
+++ src/portage/overlay/mod.rs
@@ -1,8 +1,11 @@
+mod builder;
+
 use std::collections::{BTreeMap, HashMap};
 use std::convert::TryFrom;
 use std::fs;
 use std::path::{Path, PathBuf};
-use std::sync::{Arc, Mutex};
+
+use self::builder::OverlayTableBuilder;
 
 use super::{Profile, ProfileKey};
 use crate::parse;
@@ -10,7 +13,6 @@ use crate::portage::profile::{MuncherState, ValueMuncher};
 use crate::portage::profile_parser::RVal;
 
 use anyhow::{anyhow, Context};
-use ignore::{self, DirEntry, WalkState};
 
 #[derive(Debug, Hash, Eq, PartialEq)]
 pub struct Overlay {
@@ -254,107 +256,6 @@ impl OverlayTable {
         match self.get_variable_no_inheritance(profile_key, variable)? {
             Some(v) => Ok(Some((v, profile_key))),
             None => Ok(self.get_variable_from_parents(profile_key, variable)?),
-        }
-    }
-}
-
-impl TryFrom<OverlayTableBuilder> for OverlayTable {
-    type Error = anyhow::Error;
-    fn try_from(other: OverlayTableBuilder) -> Result<Self, Self::Error> {
-        let errs = Arc::try_unwrap(other.errs).unwrap().into_inner().unwrap();
-        if !errs.is_empty() {
-            return Err(errs.into_iter().next().unwrap());
-        }
-        Ok(Arc::try_unwrap(other.table).unwrap().into_inner()?)
-    }
-}
-
-#[derive(Debug)]
-pub struct OverlayTableBuilder {
-    table: Arc<Mutex<OverlayTable>>,
-    errs: Arc<Mutex<Vec<anyhow::Error>>>,
-}
-
-impl OverlayTableBuilder {
-    pub fn new() -> Self {
-        Self {
-            table: Arc::new(Mutex::new(OverlayTable::new())),
-            errs: Arc::new(Mutex::new(Default::default())),
-        }
-    }
-}
-
-impl<'s> ignore::ParallelVisitorBuilder<'s> for OverlayTableBuilder {
-    fn build(&mut self) -> Box<dyn ignore::ParallelVisitor + 's> {
-        Box::new(OverlayTablePiece {
-            table: Arc::clone(&self.table),
-            map: HashMap::new(),
-            errs: Arc::clone(&self.errs),
-            local_errs: Default::default(),
-        })
-    }
-}
-
-#[derive(Debug)]
-pub struct OverlayTablePiece {
-    table: Arc<Mutex<OverlayTable>>,
-    errs: Arc<Mutex<Vec<anyhow::Error>>>,
-    map: HashMap<String, Overlay>,
-    local_errs: Vec<anyhow::Error>,
-}
-
-impl Drop for OverlayTablePiece {
-    fn drop(&mut self) {
-        let mut table = self.table.lock().unwrap();
-        let map = std::mem::replace(&mut self.map, HashMap::new());
-        for (k, v) in map {
-            table.map.insert(k, v);
-        }
-
-        let mut errs = self.errs.lock().unwrap();
-        let local_errs = std::mem::take(&mut self.local_errs);
-        for e in local_errs {
-            errs.push(e);
-        }
-    }
-}
-
-impl ignore::ParallelVisitor for OverlayTablePiece {
-    fn visit(&mut self, entry: Result<DirEntry, ignore::Error>) -> WalkState {
-        if let Ok(dir) = entry {
-            match Overlay::try_from(dir.path()) {
-                Ok(mut overlay) => match overlay
-                    .parse_profiles()
-                    .with_context(|| format!("Failed while parsing profiles of {}", &overlay.name))
-                {
-                    Ok(_) => {
-                        for p in overlay.profiles.values_mut() {
-                            match p.parse_conf().with_context(|| {
-                                format!(
-                                    "Failed while parsing {:?}:{} conf!",
-                                    dir.path().components().last().unwrap(),
-                                    p.name
-                                )
-                            }) {
-                                Ok(_) => continue,
-                                Err(e) => {
-                                    self.local_errs.push(e);
-                                    return WalkState::Quit;
-                                }
-                            }
-                        }
-                        self.map.insert(overlay.name.clone(), overlay);
-                        WalkState::Skip
-                    }
-                    Err(e) => {
-                        self.local_errs.push(e);
-                        WalkState::Quit
-                    }
-                },
-                Err(_) => WalkState::Continue,
-            }
-        } else {
-            WalkState::Continue
         }
     }
 }
